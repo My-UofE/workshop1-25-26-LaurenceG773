5cadd44072ef59108f61dc651281d412450abfce
LaurenceG773 <lg773@exeter.ac.uk>
2026-01-18 16:04:25 +0000

    fixed split using floor division

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    v = round(((len(poss_values)-1)/2))
+    v = (len(poss_values)) // 2
     return poss_values[v]
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
